048253a920f5719451e399ec9acf19e5b08ac4a2
Misty <87528367+MistyCheats@users.noreply.github.com>
2021-07-17 16:38:33 +0100

    Add files via upload

---
+++ massdm.py
@@ -0,0 +1,16 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_connect():
+  for user in client.user.friends:
+    try:
+      await user.send("TEXT IN HERE")
+      print(f"messaged: {user.name}")
+    except:
+       print(f"couldnt message: {user.name}")
+
+client.run('TOKEN HERE', bot=False)
+
+
